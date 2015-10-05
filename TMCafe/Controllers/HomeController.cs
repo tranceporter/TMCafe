cc21e6b193bb8eeba807fdf0d2ceca147a8aea7d
tranceporter <shreyas.zanpure@gmail.com>
2015-10-05 10:19:07 +0100

    Refreshed nuget packages

--- TMCafe/Controllers/HomeController.cs
+++ TMCafe/Controllers/HomeController.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TMCafe.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
